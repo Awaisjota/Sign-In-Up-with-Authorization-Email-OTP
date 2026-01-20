fbe1eb37ac059e3305a881016e63625db130cece
Awaisjota <mhrawaisjota@gmail.com>
2026-01-20 19:33:24 +0500

    change the PORT

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ app.use(
 );
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is runing on http://localhost:${PORT}`);
 });
