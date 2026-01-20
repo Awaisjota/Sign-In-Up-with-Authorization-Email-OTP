2fa627abd642447cfe5a9738d08ea044506c2f3e
Awaisjota <mhrawaisjota@gmail.com>
2026-01-20 21:02:22 +0500

    changing in api and also in server for url fixes

--- FrontEnd/src/api/Api.js
+++ FrontEnd/src/api/Api.js
@@ -2,11 +2,12 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "https://sign-in-up-with-authorization-email-otp-production.up.railway.app/",
+    process.env.NODE_ENV === "production"
+      ? "https://sign-in-up-with-authorization-email-otp-production.up.railway.app/api"
+      : "http://localhost:5000/api", // only if you run locally
   withCredentials: true,
 });
 
-// OPTIONAL: simple response error pass-through
 api.interceptors.response.use(
   (response) => response,
   (error) => Promise.reject(error)
